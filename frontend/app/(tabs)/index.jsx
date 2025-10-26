746aac72d3af3463ce9418280548585c98d7cceb
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-26 23:46:46 +0200

    add: search and banner sections

--- frontend/app/(tabs)/index.jsx
+++ frontend/app/(tabs)/index.jsx
@@ -1,11 +1,72 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TextInput, TouchableOpacity } from 'react-native'
+import { homeStyles } from '../../assets/styles/homeStyles';
+import { searchStyles } from '../../assets/styles/searchStyles';
+import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '../../constants/colors';
+import { useState } from 'react';
+import {Image} from "expo-image";
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
+
+    const [products, setProducts] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [categories, setCategories] = useState([]);
+
+    return (
+        <View style={homeStyles.container}>
+            <View style={searchStyles.searchSection}>
+                <View style={searchStyles.searchContainer}>
+                    <Ionicons 
+                        name="search" 
+                        size={20} 
+                        color={COLORS.textLight} 
+                        style={searchStyles.searchIcon} 
+                    />
+                    <TextInput 
+                        style={searchStyles.searchInput}
+                        placeholder="Search products"
+                        placeholderTextColor={COLORS.textLight}
+                        value={searchQuery}
+                        onChangeText={setSearchQuery}
+                        returnKeyType='search'
+                    />
+
+                    {searchQuery.length > 0 && (
+                        <TouchableOpacity
+                            onPress={() => setSearchQuery("")}
+                            style={searchStyles.clearButton}
+                        >
+                            <Ionicons
+                                name='close-circle'
+                                size={20}
+                                color={COLORS.textLight}
+                            />
+                        </TouchableOpacity>
+                    )}
+                </View>
+            </View>
+            
+            <ScrollView
+                contentContainerStyle={homeStyles.scrollContent}
+                showsVerticalScrollIndicator={false}
+            >
+                {/* Image */}
+                <View style={homeStyles.bannerImageContainer}>
+                    <Image 
+                        source={require("../../assets/images/home_banner.png")}
+                        style={homeStyles.bannerImage}
+                        contentFit='contain'
+                        transition={500}
+                    />
+                </View>
+
+                {/* Categories with add button */}
+
+                {/* render products */}
+            </ScrollView>
+        </View>
+    )
 }
 
-export default HomeScreen
+export default HomeScreen;
