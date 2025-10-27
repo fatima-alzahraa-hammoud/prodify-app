80aaa7fb6b917469fd1cd5582bf34a190d6625b1
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 19:54:43 +0200

    fix: getProducts API

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,16 +1,18 @@
-import {db} from "../config/db.js";
+import { eq } from "drizzle-orm";
+import { db } from "../config/db.js";
 import { productsTable } from "../db/schema.js";
 import { throwError } from "../utils/throwError.js";
 
 export const getProducts = async (req, res) => {
-    const {userId} = req.body;
-    if (!userId){
-        return throwError({message: "userId is required", res, status: 400});
+    const { userId } = req.body;
+    if (!userId) {
+        return throwError({ message: "userId is required", res, status: 400 });
     }
     try {
-        const products = await db.select().from(productsTable).where(eq(productsTable.userId, userId));
-        res.status(200).json({products});
+        const userProducts = await db.select().from(productsTable).where(eq(productsTable.userId, userId));
+        
+        res.status(200).json({ userProducts });
     } catch (error) {
-        return throwError({message: "Failed to fetch products", res, status: 500});
+        return throwError({ message: "Failed to fetch products", res, status: 500 });
     }
 };
