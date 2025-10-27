9f6b78000cd6160e2a1d2a3b98aeb8fd7f5c7ade
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 18:34:12 +0200

    add: products route to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import productRoutes from "./routes/products.routes.js";
 
 const app = express();
-app.use(express.json());
-
 const port = ENV.PORT;
+
+app.use(express.json());
+app.use("/product", productRoutes);
 
 app.listen(port, () => {
     console.log("Server is running on Port: ", port);
