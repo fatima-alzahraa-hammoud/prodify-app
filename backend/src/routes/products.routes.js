efca34452caab5409efb74141c39ac4abe4aaaf1
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 19:52:12 +0200

    add: authenticate middleware to products routes

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getProducts } from "../controllers/products.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getProducts); // get products for a user
+router.get("/", authenticate, getProducts); // get products for a user
 
 export default router;
