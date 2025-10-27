f9d8c038f25ee43793bec55f6c55ef418c14060a
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 21:24:05 +0200

    add: deleteProduct API

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -40,3 +40,35 @@ export const getProductById = async (req, res) => {
         return throwError({ message: "Failed to fetch product", res, status: 500 });
     }
 }
+
+export const deleteProduct = async (req, res) => {
+    const { userId, productId } = req.body;
+
+    if (!userId) {
+        return throwError({ message: "userId is required", res, status: 400 });
+    }
+    if (!productId) {
+        return throwError({ message: "productId is required", res, status: 400 });
+    }
+
+    try {
+        const product = await db.select().from(productsTable).where(eq(productsTable.userId, userId), eq(productsTable.id, Number(productId)));
+
+        if (!product || product.length === 0) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        const result = await db.delete(productsTable).where(eq(productsTable.userId, userId), eq(productsTable.id, Number(productId)));
+
+        if (result.count === 0) {
+            return res.status(404).json({ message: "Product not found or already deleted" });
+        }
+
+        res.status(200).json({ message: "Product deleted successfully" });
+
+    } catch (error) {
+        console.error("Error deleting product:", error);
+        return throwError({ message: "Failed to delete product", res, status: 500 });
+    }
+
+}
