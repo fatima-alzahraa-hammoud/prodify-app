4f02465489f3ec8abb3a59ee20d43e5c825460f8
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 21:10:01 +0200

    add: getProductById route

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getProducts } from "../controllers/products.controller.js";
+import { getProductById, getProducts } from "../controllers/products.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", authenticate, getProducts); // get products for a user
+router.get("/product", authenticate, getProductById); // get a product for a user 
 
 export default router;
