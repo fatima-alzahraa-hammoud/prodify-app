282b86c57e5ec5d03d68aead35241d3d4d6c4f85
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 23:53:58 +0200

    add: userId to category table

--- backend/src/db/schema/category.js
+++ backend/src/db/schema/category.js
@@ -2,6 +2,7 @@ import {pgTable, serial, text, timestamp} from "drizzle-orm/pg-core";
 
 export const categoryTable = pgTable("categories", {
     id: serial("id").primaryKey(),
+    usedId: text("user_id").notNull(),
     category: text("category").notNull(),
     image: text("image").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
