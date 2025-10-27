5fd8ab7e381a0d6d1cfdeced9871b77830f8d4a9
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-28 00:06:20 +0200

    add: createCategory route

--- backend/src/routes/categories.routes.js
+++ backend/src/routes/categories.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { getCategories } from "../controllers/categories.controller.js";
+import { createCategory, getCategories } from "../controllers/categories.controller.js";
+import { upload } from "../config/multer.js";
 
 const router = express.Router();
 
 router.get("/", authenticate, getCategories); // get categories for a user
+router.post("/", authenticate, upload.single("image"), createCategory);
 
 export default router;
