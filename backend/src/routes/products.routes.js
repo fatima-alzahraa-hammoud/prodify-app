ca4e8a7891a4f386848dcbd2512a1581cf5f7717
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 21:24:34 +0200

    add: deleteProduct router

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getProductById, getProducts } from "../controllers/products.controller.js";
+import { deleteProduct, getProductById, getProducts } from "../controllers/products.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/", authenticate, getProducts); // get products for a user
 router.get("/product", authenticate, getProductById); // get a product for a user 
+router.delete("/", authenticate, deleteProduct); // delete a product for a user
 
 export default router;
