4a42b2fd22afa7a02daa38c70ec846d0949dde48
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 23:13:01 +0200

    add: editProduct route

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProduct, deleteProduct, getProductById, getProducts } from "../controllers/products.controller.js";
+import { createProduct, deleteProduct, editProduct, getProductById, getProducts } from "../controllers/products.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 import { upload } from "../config/multer.js";
 
@@ -9,5 +9,6 @@ router.get("/", authenticate, getProducts); // get products for a user
 router.get("/product", authenticate, getProductById); // get a product for a user 
 router.delete("/", authenticate, deleteProduct); // delete a product for a user
 router.post("/", authenticate, upload.array("images", 10), createProduct);
+router.put("/", authenticate, upload.array("images", 10), editProduct);
 
 export default router;
