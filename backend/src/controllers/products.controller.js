b600d0624386af0d81f60c481d859a1fa4a4f67b
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 21:09:29 +0200

    add: getProductById API

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -10,9 +10,32 @@ export const getProducts = async (req, res) => {
     }
     try {
         const userProducts = await db.select().from(productsTable).where(eq(productsTable.userId, userId));
-        
+
         res.status(200).json({ userProducts });
     } catch (error) {
         return throwError({ message: "Failed to fetch products", res, status: 500 });
     }
 };
+
+export const getProductById = async (req, res) => {
+    const { userId, productId } = req.body;
+
+    if (!userId) {
+        return throwError({ message: "userId is required", res, status: 400 });
+    }
+    if (!productId) {
+        return throwError({ message: "productId is required", res, status: 400 });
+    }
+
+    try {
+        const product = await db.select().from(productsTable).where(eq(productsTable.userId, userId), eq(productsTable.id, Number(productId)));
+        console.log(product);
+        if (!product || product.length === 0) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json({ product: product[0] });
+    } catch (error) {
+        return throwError({ message: "Failed to fetch product", res, status: 500 });
+    }
+}
