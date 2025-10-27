9dff63f85ecd11101130065d5ecf74a815ab4600
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 23:35:45 +0200

    add: uploadDir if not exist

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,10 +1,15 @@
 import multer from "multer";
 import path from "path";
 
+const uploadDir = "uploads";
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true }); // create folder if it doesn't exist
+}
+
 // Storage configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
