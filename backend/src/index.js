eb031e8b933e65a9ccda07783fecafc966d8c0b6
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 13:15:29 +0200

    add: index.js

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { ENV } from "./config/env";
+
+const app = express();
+const port = ENV.PORT;
+
+app.listen(port, () => {
+    console.log("Server is running on Port: ", port);
+});
