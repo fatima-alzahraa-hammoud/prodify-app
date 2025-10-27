7cb17ac05f767b41d04394aaaa9036b4d470ca72
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 19:52:44 +0200

    add: clerckMiddleware to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import productRoutes from "./routes/products.routes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 const port = ENV.PORT;
 
 app.use(express.json());
+app.use(clerkMiddleware())
+
 app.use("/products", productRoutes);
+
 
 app.listen(port, () => {
     console.log("Server is running on Port: ", port);
