cb17ea9ce98fc0e3f5691f9cc3d6dc69dda3c9a4
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-27 23:59:53 +0200

    add: categoryRoutes to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import productRoutes from "./routes/products.routes.js";
+import categoryRoutes from "./routes/categories.routes.js";
 import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(clerkMiddleware())
 
 app.use("/products", productRoutes);
-
+app.use("/categories", categoryRoutes);
 
 app.listen(port, () => {
     console.log("Server is running on Port: ", port);
--- backend/src/routes/categories.routes.js
+++ backend/src/routes/categories.routes.js
@@ -5,9 +5,5 @@ import { getCategories } from "../controllers/categories.controller.js";
 const router = express.Router();
 
 router.get("/", authenticate, getCategories); // get categories for a user
-router.get("/product", authenticate, getProductById); // get a product for a user 
-router.delete("/", authenticate, deleteProduct); // delete a product for a user
-router.post("/", authenticate, upload.array("images", 10), createProduct);
-router.put("/", authenticate, upload.array("images", 10), editProduct);
 
 export default router;
