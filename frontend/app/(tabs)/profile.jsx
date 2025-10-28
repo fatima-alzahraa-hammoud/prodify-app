531b3d1896cab95515826cceedad35a0a8ebf8d6
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-28 10:50:57 +0200

    fix: profile component format

--- frontend/app/(tabs)/profile.jsx
+++ frontend/app/(tabs)/profile.jsx
@@ -1,12 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Text, View } from 'react-native'
 
 const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
+    return (
+        <View>
+            <Text>Profile</Text>
+        </View>
+    )
 }
 
 export default Profile
