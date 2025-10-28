114b46a79d902bdd4498eb53a0a8412519c32a94
fatima-alzahraa-hammoud <fatimaalzahraahammoud@gmail.com>
2025-10-28 14:29:49 +0200

    add: export to URL

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://prodify-app-6mmb.onrender.com";
+export const API_BASE_URL = "https://prodify-app-6mmb.onrender.com";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
